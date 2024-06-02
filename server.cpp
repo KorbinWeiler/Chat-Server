e9058af35fde05778a531c618f9476a6fee04a91
korbinweiler <korbinweiler@gmail.com>
2024-06-02 12:21:52 -0700

    Add ability to listen to multiple clients

--- server.cpp
+++ server.cpp
@@ -1,11 +1,19 @@
 #include "server.h"
 
 #define MAX_SIZE 1024
+#define MAX_CLIENTS 10
 #define PORT 8080
 
 int main(int argc, char const* argv[]){
 
-    int server_fd;
+    int server_fd, client_fds[MAX_CLIENTS];
+    fd_set read_fds;
+    char messageBuffer[MAX_SIZE];
+
+    //Reset all clients
+    for(int i = 0; i < MAX_CLIENTS; ++i){
+        client_fds[i] = 0;
+    }
 
     //Create server socket
     if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
@@ -24,9 +32,57 @@ int main(int argc, char const* argv[]){
         exit(EXIT_FAILURE);
     }
 
-    //Listen to the socket and accept client connections
-    listen(server_fd, 5);
-    int client_fd = accept(server_fd, nullptr, nullptr);
+    int addr_len = sizeof(address);
+    int fd;
+    int client_count;
+
+    while(1){
+        FD_ZERO(&read_fds);
+
+        FD_SET(server_fd, &read_fds);
+        int max = server_fd;
+
+        for(int i = 0; i < MAX_CLIENTS; ++i){
+            fd = client_fds[i];
+
+            if(fd > 0){
+                FD_SET(fd, &read_fds);
+            }
+
+            if(fd > client_count){
+                client_count = fd;
+            }
+        }
+
+        int activity = select(client_count + 1, &read_fds, NULL, NULL, NULL);
+        int new_socket;
+        if(FD_ISSET(server_fd, &read_fds)){
+            new_socket = accept(server_fd, (struct sockaddr*)&address, 
+            (socklen_t*)&addr_len);
+        }
+
+        for(int i = 0; i < MAX_CLIENTS; ++i){
+            if(client_fds[i] == 0){
+                client_fds[i] = new_socket;
+                break;
+            }
+        }
+
+        for(int i = 0; i < MAX_CLIENTS; ++i){
+
+            int msg_read;
+            fd = client_fds[i];
+
+            if(FD_ISSET(fd, &read_fds)){
+
+                if(msg_read = read(fd, messageBuffer, MAX_SIZE)){
+                    
+                }
+            }
+
+            
+        }
+    }
 
     close(server_fd);
     return 0;
